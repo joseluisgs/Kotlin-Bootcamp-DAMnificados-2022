7f9a5c4b05b961983bb344462b1c3330da1e555c
José Luis González <joseluisgonsan@gmail.com>
2022-07-20 19:09:49 +0200

    RaquetaMapper

--- 09-Resumen/src/main/kotlin/mappers/RaquetasMapper.kt
+++ 09-Resumen/src/main/kotlin/mappers/RaquetasMapper.kt
@@ -4,6 +4,19 @@ import dto.RaquetaDto
 import models.Raqueta
 import java.util.*
 
+
+/**
+ * Extensiones de RaquetaDto a Raqueta
+ */
 fun RaquetaDto.fromRaquetaDtoToRaqueta(): Raqueta {
     return Raqueta(UUID.fromString(id), marca, modelo, precio, peso)
+}
+
+/**
+ * Extension de String CSV a RaquetaDto
+ */
+fun String.fromCsvLineToRaquetaDto(): RaquetaDto {
+    val (id, marca, modelo, precio, peso) = this.split(',')
+    return RaquetaDto(id, marca, modelo, precio.toDouble(), peso.toInt())
+
 }
