ca75da7730ac62ded18776a72cfedfb87d182551
José Luis González <joseluisgonsan@gmail.com>
2022-07-20 21:28:07 +0200

    Refactor

--- 09-Resumen/src/main/kotlin/ResumenApp.kt
+++ 09-Resumen/src/main/kotlin/ResumenApp.kt
@@ -5,7 +5,6 @@ import services.raquetas.StorageRaquetasCsvService
 import services.raquetas.StorageRaquetasCsvServiceImpl
 import services.tenistas.StorageTenistasCsvService
 import services.tenistas.StorageTenistasCsvServiceImpl
-import java.io.File
 
 /**
  * Clase de de la Aplicación
@@ -13,6 +12,10 @@ import java.io.File
 private val logger = KotlinLogging.logger {}
 
 const val APP_PROPERTIES = "src/main/resources/config.properties"
+const val TENISTAS_INPUT_CSV_FILE = "data/tenistas.csv"
+const val RAQUETAS_INPUT_CSV_FILE = "data/raquetas.csv"
+const val RAQUETAS_OUTPUT_FILE = "data/raquetas-output.csv"
+const val TENISTAS_OUTPUT_FILE = "data/tenistas-output.csv"
 
 class ResumenApp : KoinComponent {
     var appConfig: AppConfig = AppConfig.fromPropertiesFile(APP_PROPERTIES)
@@ -26,10 +29,11 @@ class ResumenApp : KoinComponent {
 
         // Creamos los servicios que necesitamos
         val raquetasCsvStorage: StorageRaquetasCsvService = StorageRaquetasCsvServiceImpl()
-        val raquetas = raquetasCsvStorage.loadFromFile("data${File.separator}raquetas.csv")
+        val raquetas = raquetasCsvStorage.loadFromFile(RAQUETAS_INPUT_CSV_FILE)
         println("Raquetas: $raquetas")
         val tenistasCsvStorage: StorageTenistasCsvService = StorageTenistasCsvServiceImpl()
-        val tenistas = tenistasCsvStorage.loadFromFile("data${File.separator}tenistas.csv")
+        val tenistas = tenistasCsvStorage.loadFromFile(TENISTAS_INPUT_CSV_FILE)
         println("Tenistas: $tenistas")
+        raquetasCsvStorage.saveToFile(RAQUETAS_OUTPUT_FILE, raquetas)
     }
 }
